4be79ac2ed4261011e0ce631cfaa513b5d971f82
Kinkku <kinkkujuustovoileipa@gmail.com>
2020-11-26 20:07:49 +0100

    F-16 & F/A-18 liveries

---
+++ F-16C_50/X51 VC/description.lua
@@ -0,0 +1,81 @@
+livery = {
+{"F16_bl50_Engine", DIFFUSE, "f16_bl50_engine", true};
+{"F16_bl50_Engine", ROUGHNESS_METALLIC, "f16_bl50_engine_roughmet", true};
+{"F16_bl50_wing_L", DIFFUSE, "../X51/f16_bl50_wing_l", false};
+{"F16_bl50_wing_L", ROUGHNESS_METALLIC, "../X51/f16_bl50_wing_l", false};
+{"F16_bl50_wing_R", DIFFUSE, "../X51/f16_bl50_wing_r", false};
+{"F16_bl50_wing_R", ROUGHNESS_METALLIC, "../X51/f16_bl50_wing_r", false};
+{"F16_bl50_Main_1", DIFFUSE, "f16_bl50_main_1", false};
+{"F16_bl50_Main_1", ROUGHNESS_METALLIC, "f16_bl50_main_1", false};
+{"F16_bl50_Kil", DIFFUSE, "f16_bl50_kil_template", false};
+{"F16_bl50_Kil", ROUGHNESS_METALLIC, "f16_bl50_kil_template", false};
+{"F16_bl50_Main_3", DIFFUSE, "../X51/f16_bl50_main_3", false};
+{"F16_bl50_Main_3", ROUGHNESS_METALLIC, "../X51/f16_bl50_main_3", false};
+{"F16_bl50_Main_2", DIFFUSE, "../X51/f16_bl50_main_2", false};
+{"F16_bl50_Main_2", ROUGHNESS_METALLIC, "../X51/f16_bl50_main_2", false};
+{"PTB_300Gal", 0, "../X51/Fuel_Tank_300Gal_IAF_115", false};
+{"Fuel_Tank_300Gal", 1,	"Fuel_Tank_300Gal_Normal", true};
+{"Fuel_Tank_300Gal", ROUGHNESS_METALLIC, "Fuel_Tank_300Gal_IAF_RoughMet", true};
+{"PTB_300Gal", 0, "../X51/Fuel_Tank_300Gal", false};
+{"Tank_370", 0, "../X51/Fuel_Tank_370Gal", false};
+{"F_16_Tank_370", 1, "Fuel_Tank_370gal_NM", true};
+{"F_16_Tank_370", ROUGHNESS_METALLIC, "Fuel_Tank_370Gal_IAF_RoughMet",	true};
+{"LAU_129", 0, "../X51/LAU_129_diff", false};
+{"LAU_129", 1,  "LAU_129_NM", true};
+{"LAU_129", ROUGHNESS_METALLIC, "LAU_129_diff_RoughMet", true}; 
+{"F16_bl50_Wing_Pylon_1", 0, "../X51/F16_bl50_Wing_Pylon_1", false};
+{"F16_bl50_Wing_Pylon_1", 1, "f16_bl50_wing_pylon_1_normal", true};
+{"F16_bl50_Wing_Pylon_1", ROUGHNESS_METALLIC, "f16_bl50_wing_pylon_1_roughmet", true};
+{"F16_bl50_Wing_Pylon_2", 0, "../X51/F16_bl50_Wing_Pylon_2", false};
+{"F16_bl50_Wing_Pylon_2", 1, "f16_bl50_wing_pylon_2_normal", true};
+{"F16_bl50_Wing_Pylon_2", ROUGHNESS_METALLIC, "f16_bl50_wing_pylon_2_roughmet", true};
+{"F16_bl50_GLASS", 0, "F16_bl50_Glass", true};
+{"pilot_F16_patch", DIFFUSE, "empty", true};
+{"pilot_F16_patch", ROUGHNESS_METALLIC, "empty", true};
+{"pilot_F16_helmet", 0 ,"pilot_F16_helmet",true};
+{"pilot_F16", 0 ,"pilot_F16",true};
+
+--F16_bl50_MAT1_BORT_NUMBER_DECAL
+    {"F16_bl50_MAT1_BORT_NUMBER_DECAL",	0,	                "empty",		true};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	1,	                "f16_bl50_main_1_normal",	true};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	ROUGHNESS_METALLIC,	"f16_bl50_main_1_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	DECAL,	            "empty", true};
+	
+--F16_bl50_MAT1_BORT_NUMBER
+    {"F16_bl50_MAT1_BORT_NUMBER",	0,	                "empty",		true};
+	{"F16_bl50_MAT1_BORT_NUMBER",	1,	                "f16_bl50_main_2_normal",	true};
+	{"F16_bl50_MAT1_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_main_2_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	DECAL,	            "empty", true};
+
+--F16_bl50_MAT1_BORT_NUMBER_X100
+	{"F16_bl50_MAT1_BORT_NUMBER_X100", DIFFUSE, "empty", true};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100", NORMAL_MAP, "f16_bl50_main_2_normal", true};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100", ROUGHNESS_METALLIC, "f16_bl50_main_2_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100", DECAL, "empty", true};
+
+--f16_bort_number
+    {"f16_bort_number",	0,	                "empty",			true};
+	{"f16_bort_number",	1,	                "f16_bl50_main_2_normal",	true};
+	{"f16_bort_number",	ROUGHNESS_METALLIC,	"f16_bl50_main_2_roughmet", false};
+	{"f16_bort_number",	DECAL,	            "empty", true};
+
+--f16c intake
+    {"F16_bl50_INTAKE_BORT_NUMBER",0,"empty",true};
+	{"F16_bl50_INTAKE_BORT_NUMBER",1,"f16_bl50_main_3_normal",true};
+	{"F16_bl50_INTAKE_BORT_NUMBER",ROUGHNESS_METALLIC,"f16_bl50_main_3_RoughMet",false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",DECAL,"empty",true};
+-- fin bort number
+ {"F16_bl50_FIN_BORT_NUMBER",	0,"empty",	true};
+	{"F16_bl50_FIN_BORT_NUMBER",	1,"f16_bl50_kil_normal",	true};
+	{"F16_bl50_FIN_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_kil_roughmet",	false};
+	{"F16_bl50_FIN_BORT_NUMBER",	DECAL,	            "empty",	true};
+	
+--F16_bl50_FIN_DECAL
+    {"F16_bl50_FIN_DECAL",	0,	              "empty",				true};
+	{"F16_bl50_FIN_DECAL",	1,	                "f16_bl50_kil_normal",		true};
+	{"F16_bl50_FIN_DECAL",	ROUGHNESS_METALLIC,	"f16_bl50_kil_roughmet",	false};
+	{"F16_bl50_FIN_DECAL",	DECAL,	            "empty",	true};
+}
+
+name = "X51 VC"
+
