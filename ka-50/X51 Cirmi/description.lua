d51187810efddd277cafda3684488b86f87d01ba
Kinkku <kinkkujuustovoileipa@gmail.com>
2020-11-26 20:07:13 +0100

    Added Ka-50 test

---
+++ ka-50/X51 Cirmi/description.lua
@@ -0,0 +1,23 @@
+livery = {
+	{"KA-50-lamp", 0, "KA-50-PAINT1", false},
+	{"KA-50-PAINT1", 0, "KA-50-PAINT2", false},
+	{"KA-50-PAINT1_M", 0, "KA-50-PAINT2", false},
+	{"KA-50-PAINT2", 0, "KA-50-PAINT1", false},
+	{"KA-50-PAINT3", 0, "KA-50-PAINT1-DEF-03", false},
+	{"KA-50-PAINT4", 0, "KA-50-PAINT4", false},
+	{"KA-50-PAINT5", 0, "KA-50-PAINT5", false},
+	{"KA-50-PAINT6", 0, "KA-50-PAINT6", false},
+	{"KA-50-Vtulka_met", 0, "KA-50-PAINT1", false},
+	{"SOPLA_KA-50-PAINT3", 0, "KA-50-PAINT1-DEF-03", false},
+	{"KA-50-gear-rubber", 0, "KA-50-PAINT5", false},
+	{"KA-50_ST-rm_paint1", 0, "Ka-50_St_Paint1", false},
+	{"KA-50_Lifting_rotor_mast", 0, "KA-50-LRM", false},
+	{"KA-50_LRM_Part", 0, "KA-50-LRMpart", false},
+	{"KA-50_LRM-CENTRE", 0, "KA-50-LRM_centre", false},
+	{"KA-50-LRM_B", 0, "KA-50-LRM_B", false},
+	{"KA-50_BORT_NUMBERs", 0, "ka-50_numbers_yr", false},
+}
+id = 10
+name = "=X51= Cirmi"
+
+
